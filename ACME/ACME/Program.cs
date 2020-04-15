7ad1166ff2980b22b2aa8e1bca8c7abe7ac44815
Brett Dyer <brettpatrick4@gmail.com>
2020-04-15 11:36:32 -0700

    First C# assingments
    The first few assingments in C# course

---
+++ ACME/ACME/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine(@"Welcome to ACME Accounting Systems.
+Remmber, we're ""accounting"" on you!");
+        Console.ReadLine();
+        }
+    }
+

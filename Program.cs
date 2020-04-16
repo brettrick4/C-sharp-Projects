3c23368aad53ab3bc0f85435b4a1bf7ef2fb432d
Brett Dyer <brettpatrick4@gmail.com>
2020-04-16 16:15:06 -0700

    income comparison

--- Program.cs
+++ Program.cs
@@ -4,41 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MathDrill3.cs
+namespace IncomeComparison
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Enter a whole number between 1 and ten billion.");
-            long num1 = Convert.ToInt32(Console.ReadLine());
-            long product = num1 * 50;
-            Console.WriteLine("Your number times 50 = " + product);
+            Console.WriteLine("Anonymous Income Comparison Program");
+            //Console.ReadLine();
+
+            Console.WriteLine("Person 1");
+            //Console.ReadLine();
+
+            Console.WriteLine("Hourly Rate: ");
+            int rate1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(rate1);
             Console.ReadLine();
 
-            Console.WriteLine("Enter any number, with or without a decimal point.");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            double total = num2 + 25;
-            Console.WriteLine("Your number added to 25 = " + total);
+            Console.WriteLine("Hours worked per week: ");
+            int hours1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(hours1);
             Console.ReadLine();
 
-            Console.WriteLine("Enter another number with or without a decimal point.");
-            double num3 = Convert.ToDouble(Console.ReadLine());
-            double quotient = num3 / 12.5;
-            Console.WriteLine("Your number divided by 12.5 = " + quotient);
+            Console.WriteLine("Person 2");
+
+            Console.WriteLine("Hourly Rate: ");
+            int rate2 = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
 
-            Console.WriteLine("Enter a whole number");
-            int num4 = Convert.ToInt32(Console.ReadLine());
-            int remainder = num4 % 7;
-            Console.WriteLine("Your number divided by 7 gives a remainder of " + remainder);
+            Console.WriteLine("Hours worked per week: ");
+            int hours2 = Convert.ToInt32(Console.ReadLine());
             Console.ReadLine();
 
-            Console.WriteLine("Now choose a number between 1 and 100");
-            int num5 = Convert.ToInt32(Console.ReadLine());
-            bool trueOrfalse = num5 > 50;
-            Console.WriteLine("Your number is greater than 50 = " + trueOrfalse.ToString());
+            Console.WriteLine("Annual salary of Person 1: ");
+            int yearlyIncome = (rate1 * hours1) * 52;
+            Console.WriteLine(yearlyIncome);
             Console.ReadLine();
+
+            Console.WriteLine("Annual salary of Person 2: ");
+            int annualIncome = (rate2 * hours2) * 52;
+            Console.WriteLine(annualIncome);
+            Console.ReadLine();
+
+            Console.WriteLine("Does Person 1 make more money annually than Person 2?");
+            bool trueOrfalse = yearlyIncome > annualIncome;
+            Console.WriteLine(trueOrfalse);
+            Console.ReadLine();
+
         }
     }
 }

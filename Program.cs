dbdc9626afbef38eb052dd84a9763962083adcc8
Brett Dyer <brettpatrick4@gmail.com>
2020-04-17 11:52:50 -0700

    done

--- Program.cs
+++ Program.cs
@@ -12,30 +12,26 @@ namespace CarInsurance
         {
             Console.WriteLine("What is your age?");
             int age = Convert.ToInt32(Console.ReadLine());
-            
-            Console.ReadLine();
-			
-			string DUI;
-			bool isQualified = false;
+
+            string DUI; 
+            bool isQualified = false;
             Console.WriteLine("True or false; you have been charged with a DUI.");
-            DUI = Console.ReadLine()ToLower();
-			if (DUI == "false")
-			{
-				isQualified = true;
-			}
-            
+            DUI = Console.ReadLine().ToLower();
+            if (DUI == "false")
+            {
+                isQualified = true;
+            }
 
             Console.WriteLine("How many tickets do you have?");
             int tickets = Convert.ToInt32(Console.ReadLine());
 
-
-            if (age > 15 && isQualified == true && tickets < 3)
+            if (isQualified == true && tickets < 3 && age >= 15)
             {
                 Console.WriteLine("You are qualified for car insurance.");
             }
             else
             {
-                Console.WriteLine("You are not eligible for car insurance.");
+                Console.WriteLine("Sorry, you are not qualified for car insurance.");
             }
             Console.ReadLine();
         }

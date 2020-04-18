649ab3c51b973a5b9aa8f1d644498e733d6bfeb3
Brett Dyer <brettpatrick4@gmail.com>
2020-04-18 16:01:39 -0700

    Do while drill

--- Program.cs
+++ Program.cs
@@ -4,29 +4,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WhileDrill
+namespace DoWhileDrill
 {
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Enter the age of your dog.");
-            int dogAge = Convert.ToInt32(Console.ReadLine());
-            bool isPuppy = dogAge < 3 == true;
-            bool isGrown = dogAge >= 3 == true;
+
+            Console.WriteLine(@"Hot enough for ya? 
+Enter the current temperature.");
+            int roomTemp = 70;
+            int currentTemp = Convert.ToInt32(Console.ReadLine());
+            bool isTooHot = currentTemp > roomTemp;
+            bool isPerfect = currentTemp == roomTemp;
+
+            do
+            {
+                switch (currentTemp)
+                {
+                    case 60:
+                        Console.WriteLine("It's a little chilly.");
+                        Console.WriteLine("Close that window, will you? Is it getting warmer?");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 70:
+                        Console.WriteLine("70 is perfect. Don't touch that thermostat!");
+                        isPerfect = true;
+                        Console.ReadLine();
+                        return;
+                    case 80:
+                        Console.WriteLine("80 is too hot! Open a window, turn on the fan!");
+                        Console.WriteLine("Has is cooled down yet? What's the temperature now?");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 100:
+                        Console.WriteLine("It's so hot I can't even move...");
+                        Console.WriteLine("Am I dead...? Tell me it's not 100 anymore");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Hot enough for ya?");
+                        Console.WriteLine("What's the temperature now?");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (isTooHot);
             
-            while (isPuppy)
-            {
-                Console.WriteLine("Your dog is a puppy.");
-                Console.WriteLine("You should feed your dog puppy food.");
-                break;
-            }
-            while (isGrown)
-            {
-                Console.WriteLine("Your dog is old enough for adult dog food.");
-                break;
-            }
-        Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }

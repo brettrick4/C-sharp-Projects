f35e1943a18df09594fccfd82430f0f278d134fe
Brett Dyer <brettpatrick4@gmail.com>
2020-04-20 09:57:19 -0700

    While and DoWhile drills

---
+++ DoWhileDrill/DoWhileDrill/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine(@"Hot enough for ya? 
+Enter the current temperature.");
+            int roomTemp = 70;
+            int currentTemp = Convert.ToInt32(Console.ReadLine());
+            bool isTooHot = currentTemp > roomTemp;
+            bool isPerfect = currentTemp == roomTemp;
+
+            do
+            {
+                switch (currentTemp)
+                {
+                    case 60:
+                        Console.WriteLine("It's a little chilly.");
+                        Console.WriteLine("Close that window, will you? Is it getting warmer?");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 70:
+                        Console.WriteLine("70 is perfect. Don't touch that thermostat!");
+                        isPerfect = true;
+                        Console.ReadLine();
+                        return;
+                    case 80:
+                        Console.WriteLine("80 is too hot! Open a window, turn on the fan!");
+                        Console.WriteLine("Has is cooled down yet? What's the temperature now?");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 100:
+                        Console.WriteLine("It's so hot I can't even move...");
+                        Console.WriteLine("Am I dead...? Tell me it's not 100 anymore");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Hot enough for ya?");
+                        Console.WriteLine("What's the temperature now?");
+                        currentTemp = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (isTooHot);
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WhileDrill/WhileDrill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the age of your dog.");
+            int dogAge = Convert.ToInt32(Console.ReadLine());
+            bool isPuppy = dogAge < 3 == true;
+            bool isGrown = dogAge >= 3 == true;
+            
+            while (isPuppy)
+            {
+                Console.WriteLine("Your dog is a puppy.");
+                Console.WriteLine("You should feed your dog puppy food.");
+                break;
+            }
+            while (isGrown)
+            {
+                Console.WriteLine("Your dog is old enough for adult dog food.");
+                break;
+            }
+        Console.ReadLine();
+        }
+    }
+}

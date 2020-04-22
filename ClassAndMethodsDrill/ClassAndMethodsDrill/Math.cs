94be34120f0757fd5515de7f00af8a38ae86bf00
Brett Dyer <brettpatrick4@gmail.com>
2020-04-22 16:54:43 -0700

    Here is a rough draft of the ClassAndMethodsDrill

---
+++ ClassAndMethodsDrill/ClassAndMethodsDrill/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsDrill
+{
+    public class Math
+    {
+        public void Dogs(int userNum)
+        {
+            int hotDogs = userNum * 2;
+            Console.WriteLine("Do you think you could eat " + hotDogs + " before you burst?");
+        }
+
+        public void Total(int userNum)
+        {
+            int total = userNum - 1 ;
+            Console.WriteLine("We actually only have " + total + " dogs left. Want all of 'em?");
+        }
+
+        public void Buns(int hotDogs)
+        {
+            int buns = hotDogs * 2;
+            Console.WriteLine("Somehow we still have " + buns + " buns left!");
+        }
+    }
+}
---
+++ ClassAndMethodsDrill/ClassAndMethodsDrill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethodsDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many hot dogs would you like?");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+
+            Console.ReadLine();
+        }
+    }
+}

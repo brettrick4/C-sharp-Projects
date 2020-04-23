becea1ec2f8403338245f06a88af8976e4377eae
Brett Dyer <brettpatrick4@gmail.com>
2020-04-23 15:09:28 -0700

    Drill creating a class and calling methods

--- ClassAndMethodsDrill/ClassAndMethodsDrill/Math.cs
+++ ClassAndMethodsDrill/ClassAndMethodsDrill/Math.cs
@@ -10,6 +10,7 @@ namespace ClassAndMethodsDrill
     {
         public void Dogs(int userNum)
         {
+            Math math1 = new Math();
             int hotDogs = userNum * 2;
             Console.WriteLine("Do you think you could eat " + hotDogs + " before you burst?");
         }
@@ -17,13 +18,23 @@ namespace ClassAndMethodsDrill
         public void Total(int userNum)
         {
             int total = userNum - 1 ;
-            Console.WriteLine("We actually only have " + total + " dogs left. Want all of 'em?");
+            Console.WriteLine("We actually only have " + total + " left.");
+            if ( total > 1)
+            {
+                Console.WriteLine("Want the rest of 'em?");
+            }
+            else if ( total == 1)
+            {
+                Console.WriteLine("You get the last one!");
+            }
+            Console.ReadLine();
         }
 
         public void Buns(int hotDogs)
         {
             int buns = hotDogs * 2;
-            Console.WriteLine("Somehow we still have " + buns + " buns left!");
+            Console.WriteLine(@"Somehow we still have " + buns + " buns left!\n" +
+                "How does that always happen?");
         }
     }
 }
--- ClassAndMethodsDrill/ClassAndMethodsDrill/Program.cs
+++ ClassAndMethodsDrill/ClassAndMethodsDrill/Program.cs
@@ -10,11 +10,13 @@ namespace ClassAndMethodsDrill
     {
         static void Main(string[] args)
         {
+
+            Math math1 = new Math();
             Console.WriteLine("How many hot dogs would you like?");
             int userNum = Convert.ToInt32(Console.ReadLine());
-
-            Math math = new Math();
-
+            math1.Dogs(userNum);
+            math1.Total(userNum);
+            math1.Buns(userNum);
             Console.ReadLine();
         }
     }

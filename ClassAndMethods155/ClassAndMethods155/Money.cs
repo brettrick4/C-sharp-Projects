a3f664ed0087b7766cdba954f582fa658df194d1
Brett Dyer <brettpatrick4@gmail.com>
2020-04-24 18:22:22 -0700

    Classes and methods
    Rough draft fo the classes and methods drill, C# page 155

---
+++ ClassAndMethods155/ClassAndMethods155/Money.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods155
+{
+    public class Money
+    {
+        public void Dollars(int myMon)
+        {
+            int yourMon = myMon * 2 / 3;
+            int totalFunds = yourMon + myMon;
+            Console.WriteLine(@"With my " +myMon+ " and with your " +yourMon+ " that gives us " +totalFunds+ " bucks.");
+        }
+        public void Dollars(decimal dollar)
+        {
+            Console.WriteLine("Wait! Check the couch cusions. What did you find?");
+            decimal change = Convert.ToDecimal(Console.ReadLine());
+            int dollars = Convert.ToInt32(Math.Ceiling(change));
+            Console.WriteLine("Thats nearly " + dollars + "!");
+        }
+        public void Dollars(string cans, int dollars)
+        {
+            Console.WriteLine("How many bottles and cans do we have to return?");
+            string returnsStr = Convert.ToString(Console.ReadLine());
+            int returns = Convert.ToInt32(returnsStr);
+            decimal totalCans = Convert.ToDecimal(returns * .1);
+            int totalReturns = Convert.ToInt32(Math.Ceiling(totalCans));
+            int totalMoney = totalReturns + dollars;
+            Console.WriteLine("That gives us about " + totalMoney + " bucks.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassAndMethods155/ClassAndMethods155/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods155
+{
+    class Program
+    {
+        static void Main(string[] args, int dollar, int totalMoney)
+        {
+            Money money1 = new Money();
+            Console.WriteLine("Think I'll make a beer run, but all I have is...");
+            int myMon = Convert.ToInt32(Console.ReadLine());
+            money1.Dollars(myMon);
+            money1.Dollars(dollar);
+            money1.Dollars(totalMoney);
+            Console.ReadLine();
+        }
+    }
+}

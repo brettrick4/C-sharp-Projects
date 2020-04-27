0657e18f1b16ee7c426189695b2f4e74b0f1e56a
Brett Dyer <brettpatrick4@gmail.com>
2020-04-27 16:58:05 -0700

    This should work

--- Program.cs
+++ Program.cs
@@ -4,53 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DoWhileDrill
+namespace OptionalParameters
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-
-            Console.WriteLine(@"Hot enough for ya? 
-Enter the current temperature.");
-            int roomTemp = 70;
-            int currentTemp = Convert.ToInt32(Console.ReadLine());
-            bool isTooHot = currentTemp > roomTemp;
-            bool isPerfect = currentTemp == roomTemp;
-
-            do
-            {
-                switch (currentTemp)
-                {
-                    case 60:
-                        Console.WriteLine("It's a little chilly.");
-                        Console.WriteLine("Close that window, will you? Is it getting warmer?");
-                        currentTemp = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 70:
-                        Console.WriteLine("70 is perfect. Don't touch that thermostat!");
-                        isPerfect = true;
-                        Console.ReadLine();
-                        return;
-                    case 80:
-                        Console.WriteLine("80 is too hot! Open a window, turn on the fan!");
-                        Console.WriteLine("Has is cooled down yet? What's the temperature now?");
-                        currentTemp = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 100:
-                        Console.WriteLine("It's so hot I can't even move...");
-                        Console.WriteLine("Am I dead...? Tell me it's not 100 anymore");
-                        currentTemp = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    default:
-                        Console.WriteLine("Hot enough for ya?");
-                        Console.WriteLine("What's the temperature now?");
-                        currentTemp = Convert.ToInt32(Console.ReadLine());
-                        break;
-                }
-            }
-            while (isTooHot);
+            Candy candy1 = new Candy();
+            Console.WriteLine("Enter a number.");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
             
+            Console.WriteLine("Now enter a second number. Or don't, it's cool either way.");
+            string userNum2 = (Console.ReadLine());
+            if (userNum2 == "")
+            {
+                candy1.Choc(userNum1);
+            }
+            else 
+            {
+                int userNum3 = Convert.ToInt32(userNum2);
+                candy1.Choc(userNum1, userNum3);
+            }
+
             Console.ReadLine();
         }
     }

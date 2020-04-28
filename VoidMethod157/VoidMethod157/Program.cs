8212b7e8de3e99c68d23bf6acb7fcbe7c4570e80
Brett Dyer <brettpatrick4@gmail.com>
2020-04-27 17:20:49 -0700

    Void method, two parameters

---
+++ VoidMethod157/VoidMethod157/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Volume volume1 = new Volume();
+            Console.WriteLine("Are you drinking enough beer? " +
+                "How many pints of beer do you drink in a day?");
+            int pintGlass = Convert.ToInt32(Console.ReadLine());
+            int ounces = 16;
+            volume1.Beer(pintGlass, ounces);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VoidMethod157/VoidMethod157/Volume.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod157
+{
+    class Volume
+    {
+        public void Beer(int pintGlass, int ounces = 16)
+        {
+            int beerSteins = pintGlass * 2;
+            Console.WriteLine("If you drink " + pintGlass + " pints of beer, you have had " +
+                "the equivalent of " + beerSteins + " beer steins. A normal pint glass holds " 
+                 + ounces + " ounces. That's alot of beer!");
+        }
+    }
+}

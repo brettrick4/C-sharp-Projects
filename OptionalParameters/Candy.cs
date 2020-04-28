68e5c494c99220906154f39ce360e00fbd560f2c
Brett Dyer <brettpatrick4@gmail.com>
2020-04-27 17:45:58 -0700

    ?

---
+++ OptionalParameters/Candy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Candy
+    {
+        public int Choc(int userNum1, int userNum2 = 5)
+        {
+            int userTotal = userNum1 * userNum2;
+            Console.WriteLine(userNum1 + " times " + userNum2 + " equals " + userTotal);
+            return userTotal;
+        }
+    }
+}

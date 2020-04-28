f615e06b04067ef99c4c6bbd5adb3b7b6814caca
Brett Dyer <brettpatrick4@gmail.com>
2020-04-28 12:09:15 -0700

    Optional Parameters Drill

---
+++ OptionalParametersDrill/OptionalParametersDrill/Candy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParametersDrill
+{
+    class Candy
+    {
+        public int Choc(int userNum1, int userNum2 = 5)
+        {
+            int userTotal = userNmu1 * userNum2;
+            Console.WriteLine(userNum1 + " times " + userNum2 + " equals " + userTotal);
+            return userTotal;
+        }
+    }
+}
---
+++ OptionalParametersDrill/OptionalParametersDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParametersDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Candy candy1 = new Candy();
+            Console.WriteLine("Enter a number.");
+            int userNum1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Now enter a second number. Or don't, it's cool either way.");
+            string userNum2 = (Console.ReadLine());
+            if (userNum2 == "")
+            {
+                candy1.Choc(userNum1);
+            }
+            else
+            {
+                int userNum3 = Convert.ToInt32(userNum2);
+                candy1.Choc(userNum1, userNum3);
+            }
+            Console.ReadLine();
+        }
+    }
+}

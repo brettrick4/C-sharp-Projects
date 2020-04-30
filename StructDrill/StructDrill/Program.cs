40133a53ae8bd2ee4c18f3c4727a52ff48401a71
Brett Dyer <brettpatrick4@gmail.com>
2020-04-30 11:01:51 -0700

    Using a struct

---
+++ StructDrill/StructDrill/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number1;
+            number1.Amount = 12.34m;
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+        }
+    }
+}

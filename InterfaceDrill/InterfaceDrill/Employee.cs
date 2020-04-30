57d16f5b9ca8a6c4ef08d4330ef4f69fcb7df02c
Brett Dyer <brettpatrick4@gmail.com>
2020-04-29 17:57:42 -0700

    Interface drill

---
+++ InterfaceDrill/InterfaceDrill/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDrill
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + lastName);
+        }
+        public void Quittable(Employee employee)
+        {
+            Console.WriteLine("I Quit!");
+        }
+    }
+}

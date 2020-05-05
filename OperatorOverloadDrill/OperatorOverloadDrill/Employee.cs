691ec02733768c486ef20d72ae1c5658fcd5e3f2
Brett Dyer <brettpatrick4@gmail.com>
2020-05-05 13:36:25 -0700

    Operator Overload Drill

---
+++ OperatorOverloadDrill/OperatorOverloadDrill/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadDrill
+{
+    public class Employee : Person
+    {
+
+        public static bool operator ==(Employee employeeC, Employee employeeD)
+        {
+            if (employeeC.ID == employeeD.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee employeeC, Employee employeeD)
+        {
+            if (employeeC.ID != employeeD.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+    }
+}
---
+++ OperatorOverloadDrill/OperatorOverloadDrill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeA = new Employee { ID = 22, employeeA = "Joe Shmoe" };
+            Employee employeeB = new Employee { ID = 22, employeeA = "Joe Binden" };
+            
+            Console.WriteLine(employeeA.ID == employeeB.ID);
+            Console.ReadLine();
+             
+        }
+    }
+}

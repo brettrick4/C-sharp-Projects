99a835a22a5c3ef6fdd14c34191bbdf2d813f34d
Brett Dyer <brettpatrick4@gmail.com>
2020-05-05 16:24:55 -0700

    Rough draft of Generics Drill

---
+++ GenericsDrill/GenericsDrill/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDrill
+{
+    class Employee<T>
+    {
+        T _Things;
+
+        public Employee(T things)
+        {
+            this._Things = things;
+        }
+        public void Write()
+        {
+            Console.WriteLine(this._Things);
+        }
+    }
+}
---
+++ GenericsDrill/GenericsDrill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace GenericsDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Things = new List<int>();
+            Things.Add(22);
+            Things.Add(44);
+            Things.Add(88);
+
+            List<string> things = new List<string>();
+            things.Add("Here I ");
+            things.Add("go again ");
+            things.Add("on my own.");
+
+            foreach (string thing in things)
+            {
+                Console.WriteLine(things);
+            }
+            foreach (int Thing in Things)
+            {
+                Console.WriteLine(Things);
+                Console.ReadLine();
+            }
+        }
+    }
+}

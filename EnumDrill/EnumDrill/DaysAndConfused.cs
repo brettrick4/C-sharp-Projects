3f714e192fb743b434599c19ea25ae42fe9407a1
Brett Dyer <brettpatrick4@gmail.com>
2020-05-06 13:16:37 -0700

    Enums

---
+++ EnumDrill/EnumDrill/DaysAndConfused.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDrill
+{
+    public class DaysAndConfused
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ EnumDrill/EnumDrill/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace EnumDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is it?");
+            string today = Convert.ToString(Console.ReadLine().ToLower());
+            {
+                try
+                {
+                    DaysAndConfused.DaysOfTheWeek dayName = (DaysAndConfused.DaysOfTheWeek) Enum.Parse(typeof(DaysAndConfused.DaysOfTheWeek), today, true);
+                    string days = Convert.ToString(dayName);
+                    Console.WriteLine("Wow, I forgot it's {0} already.", today, Enum.Parse(typeof(DaysAndConfused.DaysOfTheWeek), days));
+                }
+
+                catch
+                {
+                    Console.WriteLine("Please enter an actual day of the week.");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}

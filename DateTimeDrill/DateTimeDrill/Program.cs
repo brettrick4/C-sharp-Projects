60ff41ec378fbbf2d16809002f27c901cf874557
Brett Dyer <brettpatrick4@gmail.com>
2020-05-07 12:04:00 -0700

    DateTime plus hours

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        //private static int Hour;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            DateTime time = DateTime.Now;
+            TimeSpan later = new TimeSpan(0, userNumber, 0, 0);
+            DateTime hoursLater = time.Add(later);
+            Console.WriteLine("Right now the time is " + time);
+            Console.WriteLine("In {0} hours, the time will be {1} ", userNumber, hoursLater);
+
+            Console.ReadLine();
+        }
+    }
+}

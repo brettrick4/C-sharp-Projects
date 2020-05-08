73872f6971053f98685942dd08a148ef452e6ef4
Brett Dyer <brettpatrick4@gmail.com>
2020-05-08 12:58:31 -0700

    Chaining Constructors
    Pretty sure I got it right this time

---
+++ ChainingConstrucorsDrill/constDrill/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Message
+{
+    public string Person;
+    public int Number;
+
+    public Message(string person) : this(person, 2)
+    {
+    }
+    public Message(string person, int number)
+    {
+        this.Person = person;
+        this.Number = number;
+    }
+}
+
---
+++ ChainingConstrucorsDrill/constDrill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "Brett";
+            var voiceMail = "You may leave a message, I may call you back...but I may not";
+            Console.WriteLine("You have reached {0}'s voice mail. " + voiceMail, myName);
+            Console.ReadLine();
+
+            Message mess = new Message("Who is calling, please?");
+            Console.WriteLine("{0} Call back in {1} hours.", mess.Person, mess.Number);
+            Console.ReadKey();
+        }
+    }
+}

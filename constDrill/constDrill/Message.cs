9bc3173f782af18228fadfb7f38afb1f3b852661
Brett Dyer <brettpatrick4@gmail.com>
2020-05-07 17:34:00 -0700

    Finally works

---
+++ constDrill/constDrill/Message.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Message
+{
+    public string Person;
+    public int Number;
+
+    public Message(string person, int num)
+    {
+        this.Person = person;
+        this.Number = num;
+    }
+
+}
+

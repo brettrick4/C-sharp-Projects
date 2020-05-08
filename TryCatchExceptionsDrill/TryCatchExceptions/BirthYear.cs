79a522e86022bc94ec6e03fdee7d64ae336ffd54
Brett Dyer <brettpatrick4@gmail.com>
2020-05-08 12:45:41 -0700

    Try/Catch take two

---
+++ TryCatchExceptionsDrill/TryCatchExceptions/BirthYear.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchExceptions
+{
+    public abstract class BirthYear
+    {
+        public DateTime Now { get; }
+        public int Year { get; }
+    }
+    
+    
+}
+    
+
+
---
+++ TryCatchExceptionsDrill/TryCatchExceptions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("How old are you?");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+
+                DateTime thisYear = DateTime.Now;
+                DateTime birthYear = thisYear.AddYears(-userAge);
+                
+                if (userAge <= 0)
+                {
+                    throw (new Exception());
+                }
+                if (userAge > 100)
+                {
+                    throw (new Exception());
+                }
+                Console.WriteLine("{0} years ago it was exactly {1}.", userAge, birthYear);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("An error occurred.");
+                Console.ReadKey();
+            }
+
+            catch 
+            {
+                Console.WriteLine("This wont work if you don't enter your real age.");
+                Console.ReadLine();
+            }
+            
+            Console.ReadLine();
+            
+        }    
+    }
+}
